9394521e2c7080b157a7afca29bfcebb32ecf3fa
electronut <info@electronut.in>
2014-07-19 09:11:24 +0530

    added generic paren

--- stacks.py
+++ stacks.py
@@ -42,12 +42,56 @@ def reverseStr(str1):
         str2 += stk.pop()
     return str2
 
+# a paranthesis checker
+def parChecker(symbolStr):
+    stk = Stack()
+    for a in symbolStr:
+        if a is '(':
+            stk.push(a)
+        elif a is ')':
+            if stk.isEmpty():
+                stk.push(a)
+            else:
+                stk.pop()
+    return stk.isEmpty()
+
+# a more generic paranthesis checker
+def parCheckerG(symbolStr):
+    stk = Stack()
+    # create a paren matching dict
+    parDict = {'}': '{', ')': '(', ']': '['}
+    for a in symbolStr:
+        if a in '{[(':
+            stk.push(a)
+        elif a in '}])':
+            if stk.isEmpty():
+                stk.push(a)
+            else:
+                # get last saved left paren
+                leftPar = stk.peek()
+                # check it matches current right paren
+                if leftPar is parDict[a]:
+                    stk.pop()
+    return stk.isEmpty()
+
 # main() function
 def main():
     print('learning stacks...')
 
     # testing reverseStr
-    print(reverseStr('yomamma'))
+    # print(reverseStr('yomamma'))
+
+    # test parenthesis
+    """
+    print(parChecker('(()wht))'))
+    print(parChecker('( ()yo() (((mama))) )'))
+    print(parChecker('(()'))
+    print(parChecker(')('))
+    """
+    # test parenthesis (generic)
+    print(parCheckerG('](yo) ] {} ()'))
+    print(parCheckerG('(yo) {[(test){0}] )}'))
+    
 
 # call main
 if __name__ == '__main__':
